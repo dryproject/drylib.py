1a77d618270781b591d7337db7498379c53bc9f3
Arto Bendiken <arto@dryproject.org>
2018-10-09 16:43:23 +0300

    Enforced the minimum required Python version.

--- src/drylib/__init__.py
+++ src/drylib/__init__.py
@@ -1,3 +1,10 @@
 # This is free and unencumbered software released into the public domain.
 
+"""DRYlib for Python."""
+
+import sys
+
+assert sys.version_info >= (3,5), \
+    "DRYlib for Python requires Python 3.5+"
+
 __all__ = []
